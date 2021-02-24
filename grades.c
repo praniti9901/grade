9c8e1145badefc17958e51cb15f7e3d98fe9ffee
praniti9901 <78950459+praniti9901@users.noreply.github.com>
2021-02-24 20:05:02 +0530

    student garde program
    we can understand grade by putting any number

---
+++ grades.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void)
+{
+int num;
+printf("Enter your mark: ");
+scanf("%d",&num);
+
+	if(num >= 85){
+	printf(" You got A grade"); 
+		}
+	else if ( num >=70)
+	    { 
+		printf(" You got B grade");
+		}
+	else if ( num >=55)
+	    {
+		printf(" You got C grade");
+		}
+	else if ( num >=40)
+	    {
+		printf(" You got D grade");
+		}
+	else if ( num <40)
+	    {
+	    printf(" You got F grade");
+		}
+return 0;
+}
